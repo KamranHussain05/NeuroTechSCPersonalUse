a34d07f1b26a1f2b6180c3f46535e24d020fd136
Kamran Hussain <kamran.hssn05@gmail.com>
2025-03-06 18:25:37 -0800

    Update radial8.py

--- Tasks/radial8.py
+++ Tasks/radial8.py
@@ -1,1 +1,14 @@
 
+# Setup the GUI with PyGame
+
+# Start neural recording, synchronize time stamps from gui and neural recording
+
+# Event loop
+  # Go cue, record time_stamp to mat 
+  # record end trial time stamp
+  # Save neural data snippet, and cue info
+  # Synchronize clocks
+
+# Save neural data to mat + save logs
+
+# Plot PSTHs
